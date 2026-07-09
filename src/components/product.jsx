da0fa6d46d528e96164b882c71b917e17cb1ba83
phanikumar30 <porallavenkataphanikumar@gmail.com>
2026-07-09 16:40:09 +0530

    Added Products

---
+++ src/components/product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function product() {
+  return (
+    <>
+    <div><h1>This is My product1</h1></div>
+    <div><h1>This is My product2</h1></div>
+    </>   
+  )
+}
+
+export default product
